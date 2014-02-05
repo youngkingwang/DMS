7dfdf3b1fceba5154f02c40cc4f2d101cc3cbb51
youngkingwang <chinawangyajun@gmail.com>
2014-02-05 21:52:53 +0800

    first commit

--- backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#remark
-
-if cp $1 $2 >&/dev/null
-then
-	exit -1
-fi
-
--- getip.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-LC_ALL=C ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' |
-cut -d: -f2 | awk '{ print $1}'
